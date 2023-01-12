b08e16485bf4cc17187a687a80d699c17d2f0773
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-01-12 16:57:31 +0600

    product-dto-added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
-import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "./modules/users/users.module";
+import { ProductsModule } from "./modules/products/products.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      process.env.DATABASE_URL ?? ""
-    ),
+    MongooseModule.forRoot(process.env.DATABASE_URL ?? ""),
     UsersModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
