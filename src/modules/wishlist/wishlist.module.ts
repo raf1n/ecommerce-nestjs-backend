4e6847851d01402ad1f0dd5237164484940ca960
Iqbal <iamhasan9501@gmail.com>
2023-01-29 12:55:53 +0600

    before pull from denji

--- src/modules/wishlist/wishlist.module.ts
+++ src/modules/wishlist/wishlist.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { WishlistService } from './wishlist.service';
-import { WishlistController } from './wishlist.controller';
+import { Module } from "@nestjs/common";
+import { WishlistService } from "./wishlist.service";
+import { WishlistController } from "./wishlist.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { WishlistSchema } from "src/schemas/wishlist.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: WishlistService.name, schema: WishlistSchema },
+    ]),
+  ],
   controllers: [WishlistController],
-  providers: [WishlistService]
+  providers: [WishlistService],
 })
 export class WishlistModule {}
