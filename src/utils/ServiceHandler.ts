0e11390f7234e1d1d87f1307221d4a3af3252813
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-02-01 17:29:23 +0600

    k

--- src/utils/ServiceHandler.ts
+++ src/utils/ServiceHandler.ts
@@ -1,14 +1,35 @@
 import { Model } from "mongoose";
 import { SearchSortDto } from "./all-queries.dto";
 
+// export class serviceHandler {
+//   static async queryHandler<T>(
+//     model: Model<T>,
+//     query: { sortBy: string; search: string; sortType: string }
+//   ): Promise<Array<T>> {
+//     let sortBy = query.sortBy;
+//     const allBrands = await model
+//       .find({ name: new RegExp(query.search, "i") })
+//       .sort({ sortBy: query.sortType });
+//     return allBrands;
+//   }
+// }
+// export class serviceHandler {
+//   static async queryHandler<T>(
+//     model: Model<T>,
+//     query: { search: string; sort: { [key: string]: SortOrder } }
+//   ): Promise<Array<T>> {
+//     const allBrands = await model
+//       .find({ name: new RegExp(query.search, "i") })
+//       .sort(query.sort);
+//     return allBrands;
+//   }
+// }
+
 export class serviceHandler {
-  static async queryHandler<T1>(
-    model: Model<T1>,
-    query: { sortBy: string; search: string; sortType: string }
-  ): Promise<Array<T1>> {
-    const allBrands = await model
+  static async queryHandler<T>(model: Model<T>, query: any): Promise<Array<T>> {
+    const allDatas = await model
       .find({ name: new RegExp(query.search, "i") })
-      .sort({ [query.sortBy]: query.sortType });
-    return allBrands;
+      .sort({ sortBy: query.sortType });
+    return allDatas;
   }
 }
