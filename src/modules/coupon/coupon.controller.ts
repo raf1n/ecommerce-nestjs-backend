d91a67e6828af1d5f21965548be60af75f3cc91f
Mubtasimur Rahman <sadabrahman275@gmail.com>
2023-02-22 17:41:07 +0600

    coupon add delete done

---
+++ src/modules/coupon/coupon.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { Query, Request } from "@nestjs/common/decorators";
+import { SearchSortDto } from "src/utils/all-queries.dto";
+import { CouponService } from "./coupon.service";
+import { CreateCouponDto } from "./dto/create-coupon.dto";
+import { UpdateCouponDto } from "./dto/update-coupon.dto";
+
+@Controller("coupon")
+export class CouponController {
+  constructor(private readonly couponService: CouponService) {}
+
+  @Post()
+  create(@Body() createCouponDto: CreateCouponDto) {
+    console.log(createCouponDto);
+    return this.couponService.create(createCouponDto);
+  }
+
+  @Get("/admin")
+  async findAllAdminCoupons(
+    @Query() query: SearchSortDto,
+    @Request() req: Request
+  ) {
+    console.log(query);
+    return await this.couponService.findAllAdminCoupons(query);
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.couponService.findAll();
+  // }
+
+  @Get(":slug")
+  findOne(@Param("slug") slug: string) {
+    return this.couponService.findOne(slug);
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateCouponDto: UpdateCouponDto) {
+    return this.couponService.update(+id, updateCouponDto);
+  }
+
+  @Delete(":slug")
+  delete(@Param("slug") slug: string) {
+    return this.couponService.delete(slug);
+  }
+
+  // @Delete(":id")
+  // remove(@Param("id") id: string) {
+  //   return this.couponService.remove(+id);
+  // }
+}
---
+++ src/modules/coupon/coupon.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Coupon, CouponDocument } from "src/schemas/coupon.schema";
+import { UtilSlug } from "src/utils/UtilSlug";
+import { CreateCouponDto } from "./dto/create-coupon.dto";
+import { UpdateCouponDto } from "./dto/update-coupon.dto";
+
+@Injectable()
+export class CouponService {
+  constructor(
+    @InjectModel(Coupon.name)
+    private readonly couponModel: Model<CouponDocument>
+  ) {}
+  // create(createCouponDto: CreateCouponDto) {
+  //   return "This action adds a new coupon";
+  // }
+
+  async create(createCouponDto: CreateCouponDto): Promise<object> {
+    createCouponDto["slug"] = UtilSlug.getUniqueId(createCouponDto.name);
+
+    const result = await new this.couponModel(createCouponDto).save();
+    if (result) {
+      return { message: "Success" };
+    }
+  }
+
+  async findAllAdminCoupons(query: any) {
+    const allCouponsData = await this.couponModel
+      .find({
+        cat_name: new RegExp(query.search, "i"),
+      })
+      .sort({ [query.sortBy]: query.sortType });
+
+    return allCouponsData;
+  }
+
+  // findAll() {
+  //   return `This action returns all coupon`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} coupon`;
+  // }
+
+  async findOne(slug: string) {
+    console.log(slug);
+    const couponFind = await this.couponModel.findOne({ slug: slug });
+    console.log(couponFind);
+    return couponFind;
+  }
+
+  update(id: number, updateCouponDto: UpdateCouponDto) {
+    return `This action updates a #${id} coupon`;
+  }
+
+  async delete(slug: string): Promise<Coupon> {
+    return await this.couponModel.findOneAndDelete({ slug }).exec();
+  }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} coupon`;
+  // }
+}
