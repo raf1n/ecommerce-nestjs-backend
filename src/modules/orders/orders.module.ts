72fa05d9c4038e2f4bdcf3c3253e90066d1bcbf3
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-03-09 17:06:46 +0600

    product-stock-update

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -4,12 +4,15 @@ import { OrdersController } from "./orders.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Order, OrderSchema } from "src/schemas/order.schema";
 import { Inventory, InventorySchema } from "src/schemas/inventory.schema";
+import { Product } from "../products/entities/product.entity";
+import { ProductSchema } from "src/schemas/product.schema";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Order.name, schema: OrderSchema },
       { name: Inventory.name, schema: InventorySchema },
+      { name: Product.name, schema: ProductSchema },
     ]),
   ],
   controllers: [OrdersController],
--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -7,6 +7,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { UtilSlug } from "src/utils/UtilSlug";
 import { Inventory, InventoryDocument } from "src/schemas/inventory.schema";
+import { Product, ProductDocument } from "src/schemas/product.schema";
 
 @Injectable()
 export class OrdersService {
@@ -14,7 +15,9 @@ export class OrdersService {
     @InjectModel(Order.name)
     private readonly orderModel: Model<OrderDocument>,
     @InjectModel(Inventory.name)
-    private readonly inventoryModel: Model<InventoryDocument>
+    private readonly inventoryModel: Model<InventoryDocument>,
+    @InjectModel(Product.name)
+    private readonly productModel: Model<ProductDocument>
   ) {}
   // ------------------post order--------------------- //
   async create(createOrderDto: CreateOrderDto): Promise<Object> {
@@ -24,17 +27,32 @@ export class OrdersService {
     const result = await new this.orderModel(createOrderDto).save();
 
     const stockProducts = createOrderDto.product_list.map(
-      (data: { slug: string; stock: number; type: "stockOut" }) => {
+      (data: { slug: string; quantity: number; type: "stockOut" }) => {
         let p = {
           product_slug: data.slug,
-          quantity: data.stock,
+          quantity: data.quantity,
           type: "stockOut",
         };
+
+        this.productModel.findOneAndUpdate(
+          { slug: data.slug },
+          {
+            $inc: {
+              stock: -data.quantity,
+            },
+          }
+        );
         return p;
       }
     );
 
     this.inventoryModel.create(stockProducts);
+
+    // const stockUpdate = createOrderDto.product_list.map(
+    //   (data: { slug: string; stock: number }) => {
+    //     console.log(data);
+    //   }
+    // );
 
     if (result) {
       return {
