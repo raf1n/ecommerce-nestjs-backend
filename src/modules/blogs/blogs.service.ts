19f5a7d4982e41a4db03a8e4ff877c2da7bf12b7
Mubtasimur Rahman <sadabrahman275@gmail.com>
2023-03-16 13:41:37 +0600

    done with showing latest blogs

--- src/modules/blogs/blogs.service.ts
+++ src/modules/blogs/blogs.service.ts
@@ -33,8 +33,23 @@ export class BlogsService {
   //   return `This action returns all blogs`;
   // }
 
-  async findAll(): Promise<Blog[]> {
-    return await this.blogModel.find({}).exec();
+  async findAll(): Promise<object> {
+    const allBlogs = await this.blogModel.find({}).exec();
+    // var todayDate = new Date();
+    // todayDate.setHours(0, 0, 0, 0);
+    // todayDate.toISOString();
+    var week = new Date();
+    week.setHours(168, 10080, 604800, 604800000);
+    const latestBlogs = await this.blogModel.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $lte: week,
+          },
+        },
+      },
+    ]);
+    return { allBlogs, latestBlogs };
   }
 
   findOne(id: number) {
