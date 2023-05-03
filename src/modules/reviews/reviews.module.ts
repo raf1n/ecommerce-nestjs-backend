5ed3d0332fd209bb1f641bd931791838a5b40946
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-05-03 13:48:23 +0600

    rating-added-in-products-upon-giving-reviews

--- src/modules/reviews/reviews.module.ts
+++ src/modules/reviews/reviews.module.ts
@@ -3,10 +3,14 @@ import { Module } from "@nestjs/common";
 import { ReviewsService } from "./reviews.service";
 import { ReviewsController } from "./reviews.controller";
 import { Review, ReviewSchema } from "src/schemas/review.schema";
+import { Product, ProductSchema } from "src/schemas/product.schema";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]),
+    MongooseModule.forFeature([
+      { name: Review.name, schema: ReviewSchema },
+      { name: Product.name, schema: ProductSchema },
+    ]),
   ],
   controllers: [ReviewsController],
   providers: [ReviewsService],
